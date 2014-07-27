41e88e7b8c53d1b8d46396fc9c4591d0aa6e494a
sunqing1001 <sunqing@yahoo.com>
2014-07-27 02:13:38 -0400

    Assignment2 Submission
    Qing Sun

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R Programming
+## Assignment 2 - Lexical Scoping
+## Author: Qing Sun
 
-## Write a short comment describing this function
+## Create special matrix in memory
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix<-function(mat=numeric()){
+    inv<-NULL
+    set<-function(y){
+        mat<<-y
+        inv<<-NULL
+    }
+    get<-function() mat
+    setInv <- function(a) inv<<-a
+    getInv <- function() inv
+    list(set=set,get=get,setInv=setInv,getInv=getInv)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Solve the inverse of matrix and save it in memory
+cacheSolve <- function(x,...) {
+    ## Return a matrix that is the inverse of 'x'
+    inv<-x$getInv()
+    if(!is.null(inv)) {
+        message("Getting cached data......")
+        return(inv)
+    }
+    else {
+        mat<-x$get()
+        inv<-solve(mat,...)
+        x$setInv(inv)
+        inv
+    }
 }
+
+
+a<-matrix(0,3,3); diag(a) <- c(1,2,3)
+a
+x<-makeCacheMatrix(a)
+x$get()
+x$getInv()
+cacheSolve(x)
+x$getInv()
+
